a335d54d80139649cdc609ceb6882d7ce7638dc6
AbihaTalat <works.abihat461@gmail.com>
2026-07-14 23:53:35 +0500

    Fix ESLint warnings

--- src/components/DestinatioData.js
+++ src/components/DestinatioData.js
@@ -1,8 +1,4 @@
 import { Component } from "react";
-import Pic1 from "../assets/bailey-zindel-NRQV-hBF10M-unsplash.jpg"
-import Pic2 from "../assets/dario-jud-9-fWN3MsWss-unsplash.jpg"
-import Pic3 from "../assets/pietro-de-grandi-T7K4aEPoGGk-unsplash.jpg"
-import Pic4 from "../assets/benjamin-voros-phIFdC6lA4E-unsplash.jpg"
 import "./DestinationStyles.css";
 
 class DestinationData extends Component{
